b916b6d0d2d7c0ef0cc16abe7ca4718b42b10fd1
camnh <nguyenhuucam91@gmail.com>
2021-09-12 20:59:07 +0700

    First commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-</head>
-<body>
-    <script src="{{ asset('js/client.js') }}"></script>
-</body>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+
+    <script src="https://p.trellocdn.com/power-up.min.js"></script>
+    <script src="./public/js/client.js"></script>
+  </body>
 </html>
